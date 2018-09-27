cd1446ff4821375f3a4f44824b8550f584094935
Hubert Kozłowski <hubert890@poczta.onet.pl>
2018-09-27 14:09:25 +0200

    fixed indentation

--- js/slider.js
+++ js/slider.js
@@ -1,37 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-var nextBtn = document.querySelector('.banner-next');
-var prevBtn = document.querySelector('.banner-prev');
-var images = document.querySelectorAll('.slide');
-var currentImageIndex = 0;
+  var nextBtn = document.querySelector('.banner-next');
+  var prevBtn = document.querySelector('.banner-prev');
+  var images = document.querySelectorAll('.slide');
+  var currentImageIndex = 0;
 
-function nextPicture() {
-	images[currentImageIndex].classList.remove('visible');
+  function nextPicture() {
+  	images[currentImageIndex].classList.remove('visible');
 
-	currentImageIndex++;
+  	currentImageIndex++;
 
-	if (currentImageIndex === images.length) {
-		currentImageIndex = 0;
-	}
+  	if (currentImageIndex === images.length) {
+  		currentImageIndex = 0;
+  	}
 
-	images[currentImageIndex].classList.add('visible');
-}
+  	images[currentImageIndex].classList.add('visible');
+  }
 
 
-function prevPicture() {
-	images[currentImageIndex].classList.remove('visible');
+  function prevPicture() {
+  	images[currentImageIndex].classList.remove('visible');
 
-	currentImageIndex--;
+  	currentImageIndex--;
 
-	if (currentImageIndex < 0) {
-		currentImageIndex = images.length - 1;
-	}
+  	if (currentImageIndex < 0) {
+  		currentImageIndex = images.length - 1;
+  	}
 
-	images[currentImageIndex].classList.add('visible');
-}
+  	images[currentImageIndex].classList.add('visible');
+  }
 
-images[currentImageIndex].classList.add('visible');
-nextBtn.addEventListener('click', nextPicture);
-prevBtn.addEventListener('click', prevPicture);
+  images[currentImageIndex].classList.add('visible');
+  nextBtn.addEventListener('click', nextPicture);
+  prevBtn.addEventListener('click', prevPicture);
 
 })
